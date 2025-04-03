31282b39e27c4885aac55fca458053b4a7488389
전창우 <2021142166@yonsei.ac.kr>
2025-04-04 00:33:44 +0900

    DB 문제 해결

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from domain.user.user_router import router as user_router
 from recommend import router as recommend_router  # ✅ RAG 추천 API 추가
-from domain.student.student_router import router as student_router
 from domain.scholarship.scholarship_router import router as scholarship_router
 from session import create_session, get_session
 
@@ -26,5 +25,4 @@ async def session_endpoint():
 # ✅ 라우터 등록
 app.include_router(user_router, prefix="/api")
 app.include_router(recommend_router)  # RAG 추천 API 라우터 연결
-app.include_router(student_router, prefix="/upload")
 app.include_router(scholarship_router, prefix="/upload")
